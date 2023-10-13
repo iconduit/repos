fec74e8145c165749a8c9abd7d704fe6acd23ab4
Erin Millard <contact@erin.id.au>
2023-10-13 14:32:46 +1000

    Uncomment variables and providers

--- provider.tf
+++ provider.tf
@@ -1,18 +1,18 @@
-# terraform {
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
 
-# provider "github" {
-#   owner = module.constants.org
+provider "github" {
+  owner = module.constants.org
 
-#   app_auth {
-#     id              = var.GITHUB_APP_ID
-#     installation_id = var.GITHUB_APP_INSTALLATION_ID
-#     pem_file        = var.GITHUB_APP_PEM_FILE
-#   }
-# }
+  app_auth {
+    id              = var.GITHUB_APP_ID
+    installation_id = var.GITHUB_APP_INSTALLATION_ID
+    pem_file        = var.GITHUB_APP_PEM_FILE
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-# module "constants" {
-#   source = "./modules/constants"
-# }
+module "constants" {
+  source = "./modules/constants"
+}
 
-# variable "GITHUB_APP_ID" {
-#   type = string
-# }
+variable "GITHUB_APP_ID" {
+  type = string
+}
 
-# variable "GITHUB_APP_INSTALLATION_ID" {
-#   type = string
-# }
+variable "GITHUB_APP_INSTALLATION_ID" {
+  type = string
+}
 
-# variable "GITHUB_APP_PEM_FILE" {
-#   type      = string
-#   sensitive = true
-# }
+variable "GITHUB_APP_PEM_FILE" {
+  type      = string
+  sensitive = true
+}
